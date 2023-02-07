c0c6a6f0cfa4fedd5b16b99027f06e79606a778e
hlila86 <hamdianoo@gmail.com>
2023-02-07 16:29:36 +0100

    tuesday update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,58 @@
+import Image from 'next/image'
 import React from 'react'
-import styles from 'styles/Navbar.module.css'
+import styles from '../styles/Navbar.module.scss'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { MdOutlineFavoriteBorder } from 'react-icons/md'
+import { GoSearch } from 'react-icons/go'
+import Link from 'next/link'
+import Topbar from './Topbar'
 
 const Navbar = () => {
   return (
-    <div className={styles.navbar}>Navbar</div>
+
+    <>
+    <Topbar />
+    <div className={styles.container}>
+
+      <div>
+        <Image src="/img/logo.webp" alt='' width={109} height={55} />
+      </div>
+
+      <div className={styles.navmenu}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+          <Link href="/">Home</Link>          </li>
+          <li className={styles.listItem}>
+            <Link href="/homme">homme</Link>
+          </li>
+          <li className={styles.listItem}>
+            <Link href="/femme">femme</Link>
+          </li>
+          <li className={styles.listItem}>
+            <Link href="/children">Children</Link>
+          </li>
+        </ul>
+
+      </div>
+
+      <div className={styles.navright}>
+
+        <div className={styles.rightItem}>
+        <GoSearch />
+        </div>
+        <div className={styles.wishlist}>
+        <MdOutlineFavoriteBorder />
+        <div>0</div>
+        </div>
+        <div className={styles.cart}>
+        <AiOutlineShoppingCart />
+        <div>0</div>
+        </div> 
+
+      </div>
+    </div>
+
+    </>
   )
 }
 
---
+++ components/Topbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/Topbar.module.scss'
+
+const Topbar = () => {
+  return (
+    <div className={styles.content}>Topbar</div>
+  )
+}
+
+export default Topbar
